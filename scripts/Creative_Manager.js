22ebab02757ebd2dd5cafb84ca8248487c11f7cc
kololoston <kololoston9871@gmail.com>
2016-02-16 08:53:54 -0500

    Creative_Manager.js
    It manages the Creative Inv

---
+++ scripts/Creative_Manager.js
@@ -0,0 +1,11 @@
+Item.setCategory(33, ItemCategory.TOOL);
+Item.setCategory(127, ItemCategory.TOOL);
+Item.setCategory(220, ItemCategory.INTERNAL);
+Item.setCategory(221, ItemCategory.INTERNAL);
+Item.setCategory(222, ItemCategory.INTERNAL);
+
+Player.addItemCreativeInv(33, 1, 0);
+Player.addItemCreativeInv(127, 1, 0);
+Player.addItemCreativeInv(220, 1, 0);
+Player.addItemCreativeInv(221, 1, 0);
+Player.addItemCreativeInv(222, 1, 0);

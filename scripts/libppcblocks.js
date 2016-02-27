dba38ea8665a4bde8c7ab5c60c7bb28c8dbfcc30
officialmcpro32 <thedeveloperguy6@gmail.com>
2016-02-27 16:07:21 +0800

    Added Block Code

---
+++ scripts/libppcblocks.js
@@ -0,0 +1,22 @@
+
+Block.defineBlock(199,"Chorus Plant",[["chorus_plant",0]]);
+
+Block.defineBlock(200,"Chorus Flowef",[["chorus_flower",0]]);
+
+Block.defineBlock(201,"Purpur Block",[["purpur_block",0]]);
+
+Block.defineBlock(202,"Purpur Pillar",[["purpur_pillar_top",0], ["purpur_pillar_top",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar_top",0], ["purpur_pillar_top",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar",0], ["purpur_pillar",0],
+["purpur_pillar_top",0], ["purpur_pillar_top",0], ["purpur_pillar",1],
+["purpur_pillar",1], ["purpur_pillar",0], ["purpur_pillar",0],
+["purpur_pillar_top",0], ["purpur_pillar_top",0], ["purpur_pillar",1],
+["purpur_pillar",1], ["purpur_pillar",1], ["purpur_pillar",1],
+["purpur_pillar",1], ["purpur_pillar",1], ["purpur_pillar_top",0],
+["purpur_pillar_top",0], ["purpur_pillar",1], ["purpur_pillar",1],
+["purpur_pillar",1], ["purpur_pillar",1], ["purpur_pillar_top",0],
+["purpur_pillar_top",0]]);
+
+Block.defineBlock(204,"tile.double_purpur.name",[["purpur_block",0]]);
+
+Block.defineBlock(205,"Purpur Slab",[["purpur_block",0]]);
+
+Block.defineBlock(206,"End Brick",[["end_bricks",0]]);
